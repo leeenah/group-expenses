d689a894c80fecde3fa9cf166da00610a03af1ad
leeenah <76856144+leeenah@users.noreply.github.com>
2021-08-04 21:40:40 -0700

    added useEffect in which it gets executed when the current accumulator value is different from previous

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Expenses.css";
 
@@ -11,6 +11,41 @@ const Expenses = ({ group, action }) => {
 
   const [isEditing, setIsEditing] = useState(false);
   const [editedGroupName, setEditedGroupName] = useState("");
+
+  useEffect(() => {
+    setExpense({
+      date: "",
+      description: "",
+      amount: "",
+    });
+  }, [group]); //[group]: accumulator
+
+  // What the useEffect does:
+  // step 1 - inital render happens
+  // step 2 - component should call useEffect but in order to do so, it needs to
+  // know if somethng changed from the last useEffect call
+  // step 2a - it compares previous value of accumulator and [group].
+  // First time, the accumulator array is [] (empty)
+  // and therefore there is a difference between [] and [group]. If there is a
+  // difference, useEffect will be executed.
+  // step 2b - it stores 'group' in the accumulator,[] to indicate previous value; next time
+  // useEffect is determining whether it should execute or not.
+  // step 3 - execute useEffect
+  // step 4 - the useEffect changes the state therefore
+  // there is a side effect of calling useEffect after the state has been changed.
+  // step 5 - compare previous value of accumulator and [group], because they're identical
+  // useEffect will not be executed.
+  // step 6 - ie click on Mexico trip, this causes a re render.
+  // step 7 - component should call useEffect but in order to do so, it needs to
+  // know if somethng changed from the last useEffect call
+  // step 7a - compares previous value of accumulator to current [group] (ie Mexico trip) to be
+  // different, useEffect will be executed.
+  // step 7b - store 'group' in the accumulator to indicate previous value; next time
+  // useEffect is determining whether it should execute or not. So now accumulator
+  // is [2601 Cambie] vs [Mexico Trip]
+  // step 8 - because were changing state, the useEffect should be called again
+  // step 9 - compare previous accumulator with [group], because there was no change, useEffect
+  // not going to be called.
 
   const handleClick = (index) => {
     let selectedExpenseItem = group.expenses[index];
@@ -26,6 +61,7 @@ const Expenses = ({ group, action }) => {
       amount: expense.amount,
     });
     action(group);
+
     setExpense({
       date: "",
       description: "",
@@ -92,13 +128,18 @@ const Expenses = ({ group, action }) => {
 
           {isEditing && (
             <form onSubmit={saveGroupName}>
-              <input type="text" onChange={typedGroupName} />
+              <input
+                type="text"
+                placeholder="Enter New Group Name"
+                onChange={typedGroupName}
+              />
             </form>
           )}
 
           <button onClick={toggleEditMode}>
-            {/** Below is a ternary condition. If isEditing state is true, "save" will be displayed, and if it's false "edit" will be displayed*/}
-            {isEditing ? "save" : "edit"}
+            {/** Below is a ternary condition. If isEditing state is true, "save" will be displayed,
+             * and if it's false "edit" will be displayed*/}
+            {isEditing ? "save" : "edit group name"}
           </button>
           {expenseItems.length > 0 && <div>{expenseItems} </div>}
           <form className="row" onSubmit={handleSubmit}>
